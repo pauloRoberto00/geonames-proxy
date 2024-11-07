a4bf05650d735a136318052e7615e15981169029
pauloRoberto00 <paulorobertoov@gmail.com>
2024-11-06 23:05:24 -0300

    pp

--- server.js
+++ server.js
@@ -3,14 +3,25 @@ import axios from 'axios';
 import cors from 'cors';
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: 'https://cuida-lar.vercel.app',
+  methods: ['GET']
+}));
 
 app.get('/geonames/findNearby', async (req, res) => {
   const { lat, lng, radius, maxRows, username } = req.query;
-  const url = `http://api.geonames.org/findNearbyPlaceNameJSON?lat=${lat}&lng=${lng}&radius=${radius}&maxRows=${maxRows}&username=${username}`;
+  const url = `http://api.geonames.org/findNearbyPlaceNameJSON`;
 
   try {
-    const response = await axios.get(url);
+    const response = await axios.get(url, {
+      params: {
+        lat: lat,
+        lng: lng,
+        username: username,
+        radius: radius,
+        maxRows: maxRows,
+      }
+    });
     res.json(response.data);
   } catch (error) {
     console.error(error);
